573cb2a21a7fb3c7f61154697bc2827ee30dd21b
David Vaillant <david@vaillant.io>
2023-12-08 15:45:33 -0800

    Hum. CUDA.

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ PAD_IDX = 0
 # Apple Silicon support.
 if torch.backends.mps.is_available():
     using_silicon = True
-    torch.set_default_device(torch.device("mps:0"))
-    DEVICE = torch.device('mps')
+    DEVICE = torch.device('mps:0')
 else:
     using_silicon = False
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.set_default_device(DEVICE)
 
 # Filter by title.
 holdouts = {
